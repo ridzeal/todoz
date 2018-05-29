d27fbb433fda456d8a31a167b8318d9f8c03a294
Rid Zeal <ridzeal@gmail.com>
2018-05-29 21:41:47 +0700

    Implement Material Design

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,41 +1,37 @@
 <template>
   <div class='ui centered card'>
-    <div class="content" v-show="!isEditing">
-      <div class='header'>
-          {{ todo.title }}
-      </div>
-      <div class='meta'>
-          {{ todo.project }}
-      </div>
-      <div class='extra content'>
-        <span class='right floated edit icon' @click="showForm">
-          <i class='edit icon'></i>edit
-        </span>
-      </div>
-    </div>
-    <div class="content" v-show="isEditing">
-      <div class='ui form'>
-        <div class='field'>
+    <md-card md-with-hover class="content" v-show="!isEditing">
+      <md-card-header>
+        <div class="md-title">{{ todo.title }}</div>
+        <div class="md-subhead" v-show="!isEditing && todo.done">Completed</div>
+        <div class="md-subhead" v-show="!isEditing && !todo.done">Pending</div>
+      </md-card-header>
+
+      <md-card-content>
+        {{ todo.project }}
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button @click="showForm">Edit</md-button>
+      </md-card-actions>
+    </md-card>
+
+    <md-card md-with-hover class="content" v-show="isEditing">
+      <md-card-content>
+        <md-field>
           <label>Title</label>
-          <input type='text' v-model="todo.title" >
-        </div>
-        <div class='field'>
+          <md-input v-model="todo.title"></md-input>
+        </md-field>
+        <md-field>
           <label>Project</label>
-          <input type='text' v-model="todo.project" >
-        </div>
-        <div class='ui two button attached buttons'>
-          <button class='ui basic blue button' @click="hideForm">
-            Close X
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class='ui bottom attached green basic button' v-show="!isEditing && todo.done" disabled>
-        Completed
-    </div>
-    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
-        Pending
-    </div>
+          <md-input v-model="todo.project"></md-input>
+        </md-field>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button @click="hideForm">Save</md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
@@ -57,3 +53,9 @@
     }
   }
 </script>
+
+<style scoped>
+.md-card {
+  margin: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Main from '@/components/Main'
 
 Vue.use(Router)
+Vue.use(VueMaterial)
 
 export default new Router({
   routes: [
