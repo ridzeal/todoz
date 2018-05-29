d2b39f48daedb76ec74208eca7dea25b9283a72a
Rid Zeal <ridzeal@gmail.com>
2018-05-29 21:00:59 +0700

    Create Todo List include edit capabilities

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <todo-list :todos="todos"></todo-list>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+import TodoList from '@/components/TodoList'
+
+export default {
+  name: 'main',
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
+  }
+}
+</script>
+<style>
+</style>  
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -8,6 +9,10 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Main',
+      component: Main
+    }, {
+      path: '/hello',
       name: 'Hello',
       component: Hello
     }
